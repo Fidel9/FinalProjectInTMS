3a400716f57079db0b829c203d56df00735a4a05
Fidel9 <dobryihomiak@gmail.com>
2022-02-26 22:12:11 +0300

    more changes

--- src/test/java/tests/ApiTest.java
+++ src/test/java/tests/ApiTest.java
@@ -2,13 +2,12 @@ package tests;
 
 import apiAdapters.ProjectAdapter;
 import apiModels.Project;
-import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 import apiResponse.ResponseStatus;
 
 import static org.testng.Assert.assertEquals;
 
-@Log4j2
+
 public class ApiTest {
 
 
@@ -20,12 +19,11 @@ public class ApiTest {
 
     @Test
     public void checkApiTest() {
-        log.info("check invalid param");
         Project project = Project.builder()
-                .code("Mouse")
-                .title("Mouse")
+                .code("Home")
+                .title("Home")
                 .build();
-        ResponseStatus actual = new ProjectAdapter().post(project, 422);
+        ResponseStatus actual = new ProjectAdapter().post(project, 200);
         ResponseStatus expected = ResponseStatus.builder()
                 .status(false)
                 .errorMessage("Data is invalid.")
